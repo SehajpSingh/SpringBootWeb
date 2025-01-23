33855ef5b978468b10a885e48d494412bb28742c
Sehaj punit Singh <sehajpunitsingh@sehajs-mbp.mynetworksettings.com>
2025-01-23 12:28:06 -0800

    working login page

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -1,28 +1,27 @@
 package com.in28minutes.springboot.myfirstwebapp.login;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
-/*
- * @Controller tells spring you need to manage this 
- * 
- * @RequestParam will help us retreive the name from URL 
- * once we have the variable, to pass it into view we will need to use model parameter
- */
 @Controller
 public class LoginController {
-	
-	private Logger logger = LoggerFactory.getLogger(getClass());
-	
-	@RequestMapping("login")
-	public String gotoLoginPage(@RequestParam String name, ModelMap model) {
-		model.put("name", name);
-		logger.debug("Request param is {}", name);
-		return "login";
-	}
+    
+    @RequestMapping(value="login", method=RequestMethod.GET)
+    public String gotoLoginPage() {
+        return "login";
+    }
+    
+    
+    @RequestMapping(value="login", method=RequestMethod.POST)
+    public String gotoWelcomePage(@RequestParam String name, 
+    		@RequestParam String password, ModelMap model) {
+        model.put("name", name);
+        model.put("password", password);
+        return "welcome";
+    }
+    
+   
 }
